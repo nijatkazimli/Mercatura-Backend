ef8c949002a350e8b8ee69e4799949489b6d142e
nijatkazimliPG <kazimli.n@pg.com>
2024-10-12 17:03:11 +0200

    fix bash script

--- wait-for-it.sh
+++ wait-for-it.sh
@@ -4,7 +4,7 @@ host="$1"
 shift
 cmd="$@"
 
-until mysql -h "$host" mysql --user=user --password=password -e "status" &> /dev/null; do
+until mysql -h "$host" --user=user --password=password -e "status" &> /dev/null; do
   >&2 echo "MySQL is unavailable - sleeping"
   sleep 1
 done
